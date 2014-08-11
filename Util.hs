dd0ae319f2274258a9559c23e71e603cb00c7398
Kris Jenkins <krisajenkins@gmail.com>
2014-08-11 08:51:20 +0100

    Simplifying the tiebreak code, per Steve's suggestion.

--- Util.hs
+++ Util.hs
@@ -7,9 +7,9 @@ module Util where
 -- | Compare with the first function. In the event of a tiebreak, compare with the next.
 -- | I expect there's a built-in for this, but I can't find it.
 tiebreak :: (a -> a -> Ordering) -> (a -> a -> Ordering) -> a -> a -> Ordering
-tiebreak f g a b
-  | f a b == EQ = g a b
-  | otherwise = f a b
+tiebreak f g a b = case f a b of
+                     EQ -> g a b
+                     other -> other
 
 -- This shouldn't be list-specific, should it? It should work with any
 -- functor. But sum seems unhappy with that idea.
